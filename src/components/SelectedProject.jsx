04e2984a91489841950eb659ac0e71b1b4e222bd
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2024-08-14 07:47:45 +0000

    feat: filter tasks based on projectId

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -12,6 +12,7 @@ export default function SelectedProject({
     month: "short",
     day: "numeric",
   });
+  const projectTasks = tasks.filter((task) => task.projectId === project.id);
   return (
     <div className="w-[35rem] mt-16">
       <header className="pb-4 mb-4 border-b-2 border-stone-300">
@@ -32,7 +33,7 @@ export default function SelectedProject({
           {project.description}
         </p>
       </header>
-      <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
+      <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={projectTasks} />
     </div>
   );
 }
